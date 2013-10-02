0d430e1122645c16fbe6b2c5af4bbcc94ecb2384
sherah <sherah@sherahsmith.com>
2013-10-02 12:22:07 -0700

    my branch

--- todolist/webapp.py
+++ todolist/webapp.py
@@ -1,3 +1,5 @@
+import flask
+import model
 from flask import redirect, url_for, render_template
 from model import app, Task
 
@@ -10,9 +12,14 @@ def home():
 
 @app.route("/add", methods=["GET"])
 def make_task():
-    return "Create a page for users to input a new task"
+    return render_template("add_tasks.html") 
 
 @app.route("/add", methods=["POST"])
 def save_task():
-    return "Save a task"
-
+	task = flask.request.form["task"]
+	notes = flask.request.form["notes"]
+	t = model.Task(task)
+	t.notes = notes
+	model.add(t)
+	model.save_all()
+	return "Thanks for adding your task."
